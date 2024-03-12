9ec07a8c47bcfe06db09db79d2e9310e632fd25a
namdoo23 <156188848+namdoo23@users.noreply.github.com>
2024-03-12 19:35:13 +0900

    Update script.js

--- docs/script/script.js
+++ docs/script/script.js
@@ -81,11 +81,11 @@ function LuxuryGoods() {
 
     effect: "coverflow",
     coverflowEffect: {
-      slideShadows: false, // 슬라이더 그림자 : 3D 효과를 강조하기 위한 회전시 흐릿한 효과
-      rotate: 0, // 슬라이더 회전 각 : 클수록 슬라이딩시 회전이 커짐
-      stretch: -40, // 슬라이더간 거리(픽셀) : 클수록 슬라이더가 서로 많이 겹침
-      depth: 150, // 깊이 효과값 : 클수록 멀리있는 느낌이 강해짐
-      modifier: 1, // 효과 배수 : 위 숫자값들에 이 값을 곱하기 처리하여 효과를 강하게 처리함
+      slideShadows: false,
+      rotate: 0,
+      stretch: -40,
+      depth: 150,
+      modifier: 1,
       scale: 1,
     },
 
